de5669f7b1dac594f5ae9a8b9b4969066f9ded18
subham <subham@driveu.in>
2020-04-17 23:11:19 +0530

    Edited emp_spec.js file

--- __tests__/api/emp_spec.js
+++ __tests__/api/emp_spec.js
@@ -1,9 +1,16 @@
 const frisby = require("frisby")
+
+
 describe("Testing Employe Data",function(){
 	it('Should be Employe data',function(){
 		return frisby.get('http://dummy.restapiexample.com/api/v1/employees')
 		.expect('status',200)
-		.expectHeader('Content-Type', 'application/json');
+		//.expectHeader('Content-Type', 'application/json');
+
+		it("Should be employee data by id",function(){
+		return frisby.get('http://dummy.restapiexample.com/api/v1/employee/1')
+		.expect('status',200)
+	});
 
 	});
 })
